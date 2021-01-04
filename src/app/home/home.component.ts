d37367e69dd3dd991dd4471bed8e6c923082e4e2
O-RGB <54633251+O-RGB@users.noreply.github.com>
2021-01-04 16:38:34 +0700

    oza

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -13,82 +13,87 @@ export class HomeComponent implements OnInit {
   innerWidth: any;
   user_id: any;
   pose_all: any;
-  like_user_all: Object=[];
-  checkLike:any;
+  like_user_all: Object = [];
+  checkLike: any;
 
-  constructor(private http: HttpClient, private router: Router,private route: ActivatedRoute) { 
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private ngZone: NgZone) {
     this.user_id = this.route.snapshot.params["id"];
+    this.innerHeight = window.innerHeight;
+    this.innerWidth = window.innerWidth;
 
+    //ตรวจจับความกว้างยาวของหน้าจอ //ตลอดเวลา//
+    window.onresize = (e: any) => {
+      this.ngZone.run(() => {
+        this.innerHeight = window.innerHeight;
+        this.innerWidth = window.innerWidth;
+      });
+    };
 
   }
 
   comment_text: any;
 
   ngOnInit(): void {
-    this.innerHeight = (window.innerHeight);
-    this.innerWidth = (window.innerWidth);
 
-    console.log( this.innerHeight +"Height");
-    console.log(  this.innerWidth  +"Width");
-    
+    var myElement = document.getElementById('element_within_div');
+    console.log(myElement);
+
     let json = { user_id: this.user_id };
     this.http.post("http://203.154.83.62:1238/select/pose_all", JSON.stringify(json)).subscribe(response => {
-      
-      
+
+
       var array = Object.values(response);
-      
-      array.forEach( (value, index) => {
+
+      array.forEach((value, index) => {
 
         //like
-         this.http.get("http://203.154.83.62:1238/select/like/"+value["pose_id"])
-          .subscribe(re =>{
+        this.http.get("http://203.154.83.62:1238/select/like/" + value["pose_id"])
+          .subscribe(re => {
             var liker = Object.values(re);
             array[index] = Object.assign({}, array[index], { liked_but: false });
             liker.forEach(element => {
-              if(element["user_id_like"] == this.user_id ){
-                      this.checkLike =  true;
-                      console.log(this.checkLike);
-                      array[index] = Object.assign({}, array[index], { liked_but: true });
-                      
-            }});
+              if (element["user_id_like"] == this.user_id) {
+                this.checkLike = true;
+                console.log(this.checkLike);
+                array[index] = Object.assign({}, array[index], { liked_but: true });
+
+              }
+            });
             array[index] = Object.assign({}, array[index], { like: liker.length });
-          }, err =>{
-            console.log("re"+ JSON.stringify(err));
-          });  
+          }, err => {
+            console.log("re" + JSON.stringify(err));
+          });
 
-          //comment
-         this.http.get("http://203.154.83.62:1238/select/comment_all/"+value["pose_id"])
-         .subscribe(re =>{
-           var comment = Object.values(re);
-           array[index] = Object.assign({}, array[index], { comment: comment.length });
-           array[index] = Object.assign({}, array[index], { comment_simple: comment[0] });
-           
-         }, err =>{
-           console.log("re"+ JSON.stringify(err));
-         }); 
-      
-      
+        //comment
+        this.http.get("http://203.154.83.62:1238/select/comment_all/" + value["pose_id"])
+          .subscribe(re => {
+            var comment = Object.values(re);
+            array[index] = Object.assign({}, array[index], { comment: comment.length });
+            array[index] = Object.assign({}, array[index], { comment_simple: comment[0] });
 
-        }); 
-      
-      
+          }, err => {
+            console.log("re" + JSON.stringify(err));
+          });
+      });
+
+
       this.pose_all = array;
       console.log();
-      
+
     }, error => {
       console.log("fail");
     });
 
   }
 
-  but_like(pose_id: string,index:any){
-    let json = { u_id: this.user_id ,pose_id: pose_id};
+  but_like(pose_id: string, index: any) {
+    let json = { u_id: this.user_id, pose_id: pose_id };
     this.http.post("http://203.154.83.62:1238/pose/like", JSON.stringify(json)).subscribe(response => {
       var pose = Object.values(this.pose_all[index]);
-      if(this.pose_all[index]["liked_but"]){
+      if (this.pose_all[index]["liked_but"]) {
         this.pose_all[index]["liked_but"] = false;
         this.pose_all[index]["like"]--;
-      }else{
+      } else {
         this.pose_all[index]["liked_but"] = true;
         this.pose_all[index]["like"]++;
       }
@@ -98,70 +103,61 @@ export class HomeComponent implements OnInit {
   }
 
 
-  
-  comment(pose_id:any) {
-      
 
-      let json = { Text: this.comment_text ,u_id: this.user_id, pose_id: pose_id};
-      this.http.post("http://203.154.83.62:1238/pose/comment", JSON.stringify(json)).subscribe(response => {
+  comment(pose_id: any) {
+    let json = { Text: this.comment_text, u_id: this.user_id, pose_id: pose_id };
+    this.http.post("http://203.154.83.62:1238/pose/comment", JSON.stringify(json)).subscribe(response => {
       console.log(response);
     }, error => {
       console.log("fail");
     });
     this.comment_text = "";
-
-    
-
-
   };
 
   display: boolean = false;
 
   showDialog() {
-      this.display = true;
+    this.display = true;
   }
 
   setMyStyles() {
-    // var elmnt = document.getElementById("myDiv")?.offsetHeight;
-    // console.log(document.getElementById("myDiv"));
-    let styles ;
-    if(this.innerWidth > 1250){
+    let styles;
+    if (this.innerWidth > 1250) {
       styles = {
-        'width':  1200+ 'px'
+        'width': 1200 + 'px'
       }
-    }else if(this.innerWidth > 900){
+    } else if (this.innerWidth > 900) {
       styles = {
-        'width':  800+ 'px'
+        'width': 900 + 'px'
       }
-    }else {
+    } else {
       styles = {
-        'width':  800+ 'px'
+        'width': 800 + 'px'
       }
     }
     return styles;
   }
   setMyClass() {
-    // var elmnt = document.getElementById("myDiv")?.offsetHeight;
-    // console.log(document.getElementById("myDiv"));
-    let Class ;
-    if(this.innerWidth > 1250){
+    let Class;
+    if (this.innerWidth > 1250) {
       Class = "p-col-6"
     }
-    else if(this.innerWidth > 900){
+    else if (this.innerWidth > 900) {
       Class = "p-col-7"
-    }else{
+    } else {
       Class = "p-col-10"
     }
     return Class;
   }
+
   setMyClassRow() {
-    let Class ;
-    if(this.innerWidth > 1250){
+    let Class;
+    if (this.innerWidth > 1250) {
       Class = "p-col"
     }
-    else if(this.innerWidth > 900){
+    else if (this.innerWidth > 900) {
       Class = "p-col-4"
-    }else{
+    } else {
       Class = "p-col-5"
     }
     return Class;
