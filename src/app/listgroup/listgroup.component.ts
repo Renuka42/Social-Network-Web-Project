c1aefc3f07a72f63ea6389285c9da0da8da7238b
T-30 <55341628+T-30@users.noreply.github.com>
2021-01-22 13:38:20 +0700

    tee

--- src/app/listgroup/listgroup.component.ts
+++ src/app/listgroup/listgroup.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListgroupComponent implements OnInit {
 
+  //หน้าจอ
+  innerHeight: any;
+  innerWidth: any;
+
+  //แสดงข้อมูล
+  user_id: any;
+  pose_all: any;
+  friend_all: any;
+  profile: any;
+  group: any;
+  comment_all: any;
+
+  //ข้อมูลชั่วคราว
+  pose_temp = 0;
+  comment_text: any;
+  checkLike: any;
+
   constructor() { }
 
   ngOnInit(): void {
+  
+  }
+
+
+  //สำหรับตั้งค่า Temp
+  setTempComment(index: any) {
+    this.pose_temp = index;
+  }
+
+  //สำหรับแสดงผลทุกหน้าจอ
+  setMyStyles() {
+    let styles;
+    if (this.innerWidth >= 1250) {
+      styles = {
+        'width': 1200 + 'px'
+      }
+    } else if (this.innerWidth >= 900) {
+      styles = {
+        'width': 900 + 'px'
+      }
+    } else if (this.innerWidth >= 600) {
+      styles = {
+        'width': 600 + 'px'
+      }
+    } else {
+      styles = {
+        'width': 104 + '%',
+      }
+    }
+    return styles;
   }
 
 }
